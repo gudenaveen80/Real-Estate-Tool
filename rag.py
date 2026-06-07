5628f2a4baa1fbe5bf2d1c8400e5c623146c514c
gudenaveen80 <gudenaveen80@gmail.com>
2026-06-08 00:19:08 +0530

    fix text splitter import

--- rag.py
+++ rag.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 from dotenv import load_dotenv
 from pathlib import Path
 from langchain_community.document_loaders import UnstructuredURLLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
